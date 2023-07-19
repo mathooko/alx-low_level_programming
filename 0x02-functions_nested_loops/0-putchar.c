b305a432d4e5fc8cfa7cf88fc3842b7e4d598f3c
mathooko <mutuasteven254@gmail.com>
2023-07-18 19:51:36 -0700

    main.h file with libraries

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,20 @@
+/**
+ * main function prints _putchar
+ */
 
+#include " main.h"
+
+int main(void)
+
+{
+    char word[9] = "_putchar";
+    int i = 0;
+
+    while (i < 8)
+    {
+        _putchar(word[i]);
+        i++;
+    }
+    _putchar('\n');
+    return (0);
+}	
